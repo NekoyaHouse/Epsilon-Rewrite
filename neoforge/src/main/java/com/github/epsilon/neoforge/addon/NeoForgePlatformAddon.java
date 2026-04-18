cc91e7800436f1c78faa24b40d3e7c2f59b35911
Chen Meng <chenmeng466@outlook.com>
2026-04-18 12:23:48 +0800

    添加 NeoForge 平台的内置模块测试功能

--- neoforge/src/main/java/com/github/epsilon/neoforge/addon/NeoForgePlatformAddon.java
+++ neoforge/src/main/java/com/github/epsilon/neoforge/addon/NeoForgePlatformAddon.java
@@ -1,6 +1,7 @@
 package com.github.epsilon.neoforge.addon;
 
 import com.github.epsilon.Epsilon;
+import com.github.epsilon.neoforge.modules.NeoModuleTest;
 
 /**
  * Built-in NeoForge addon for NeoForge-only features.
@@ -13,6 +14,7 @@ public class NeoForgePlatformAddon extends com.github.epsilon.addon.EpsilonAddon
 
     @Override
     public void onSetup() {
+        registerModule(NeoModuleTest.INSTANCE);
         Epsilon.LOGGER.info("NeoForge platform addon initialized.");
     }
 
